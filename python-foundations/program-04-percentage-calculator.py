81ff33291d95e2cae8f42048503677b1f958a5f9
paragchaurasia3824 <parag.aiml.work@gmail.com>
2026-09-15 14:30:34 +0530

    Add Python fundamentals program 4

---
+++ python-foundations/program-04-percentage-calculator.py
@@ -0,0 +1,13 @@
+sub1 = float(input("enter you marks in subject1: "))
+
+sub2 = float(input("enter your marks in subject2: "))
+
+sub3 = float(input("enter your marks in subject3: "))
+
+sub4 = float(input("enter your marks in subject4: "))
+
+sub5 = float(input("enter your marks in subject5: "))
+
+percentage = ((sub1 + sub2 + sub3 + sub4 + sub5) / 500) * 100
+
+print("your percentage is: ", percentage)
