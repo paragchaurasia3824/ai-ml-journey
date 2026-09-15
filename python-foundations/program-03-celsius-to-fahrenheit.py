25dddb29c2ec8f37b3f29df67fb41f2c10d8e296
paragchaurasia3824 <parag.aiml.work@gmail.com>
2026-09-15 14:30:29 +0530

    Add Python fundamentals program 3

---
+++ python-foundations/program-03-celsius-to-fahrenheit.py
@@ -0,0 +1,5 @@
+# (degree * (9/5)) + 32 = temp. in fahrenheit
+
+degree = float(input("enter your experienced temperature in degree celcius: "))
+conversion = (degree * (9/5)) + 32
+print("temp. in fahrenheit: ", conversion)
